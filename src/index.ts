9644c6d6dcaaa806527aaa98439dac9075b3f98c
WojtekP <wojciech.paczula@gmail.com>
2020-04-06 14:11:30 +0200

    Add redis

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,11 @@ import { ApolloServer } from 'apollo-server-express';
 import * as express from 'express';
 import { buildSchema } from 'type-graphql';
 import { createConnection } from 'typeorm';
+import * as connectRedis from 'connect-redis';
+import * as session from 'express-session';
+import * as cors from 'cors';
 import RegisterResolver from './users/resolvers/register';
+import redis from './redis';
 
 const main = async () => {
   await createConnection();
@@ -12,10 +16,35 @@ const main = async () => {
     resolvers: [RegisterResolver],
   });
 
-  const apolloServer = new ApolloServer({ schema });
+  const apolloServer = new ApolloServer({
+    schema,
+    context: ({ req }) => ({ req }),
+  });
 
   const app = express();
-
+  app.use(
+    cors({
+      credentials: true,
+      origin: 'http://localhost:3000',
+    }),
+  );
+  const RedisStore = connectRedis(session);
+  app.use(
+    session({
+      store: new RedisStore({
+        client: redis,
+      }),
+      name: 'qid',
+      secret: 'aslkdfjoiq12312',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 365, // 7 years
+      },
+    }),
+  );
   apolloServer.applyMiddleware({ app });
 
   app.listen(4000, () => {
